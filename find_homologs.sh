926b337b7e2e7eb740cc7f4aa0bf14a6c18b0f09
Onyinye <onychukka@gmail.com>
2023-09-12 19:30:58 -0400

    Performs a protein-nucleotide blast search for Vibrio_cholerae_N16961

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-tblastn -query "HK_domain.faa" -subject "Pseudomonas_aeruginosa_UCBPP-PA14.fna" -task tblastn -outfmt '6 std qlen' | awk '$3 > 30  && $4 > 0.9*$13' >> hits_Pseudomonas_aeruginosa_UCBPP-PA14.fna
-wc -l hits_Pseudomonas_aeruginosa_UCBPP-PA14.fna
-#head hits_Pseudomonas_aeruginosa_UCBPP-PA14.fna
+tblastn -query "HK_domain.faa" -subject "Vibrio_cholerae_N16961.fna" -task tblastn -outfmt '6 std qlen' | awk '$3 > 30  && $4 > 0.9*$13' >> hits_Vibrio_cholerae_N16961.fna
+wc -l hits_Vibrio_cholerae_N16961.fna
+#head hits_Vibrio_cholerae_N16961.fna
+
 
