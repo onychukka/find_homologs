d4721d96c50e55c641b188c6b7388f3dc19c6b90
Onyinye <onychukka@gmail.com>
2023-09-12 19:41:26 -0400

    Performs a protein-nucleotide blast search for Wolbachia

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-tblastn -query "HK_domain.faa" -subject "Vibrio_cholerae_N16961.fna" -task tblastn -outfmt '6 std qlen' | awk '$3 > 30  && $4 > 0.9*$13' >> hits_Vibrio_cholerae_N16961.fna
-wc -l hits_Vibrio_cholerae_N16961.fna
-#head hits_Vibrio_cholerae_N16961.fna
+tblastn -query "HK_domain.faa" -subject "Wolbachia.fna" -task tblastn -outfmt '6 std qlen' | awk '$3 > 30  && $4 > 0.9*$13' >> hits_Wolbachia.fna
+wc -l hits_Wolbachia.fna
+#head hits_Wolbachia.fna
+
 
 
