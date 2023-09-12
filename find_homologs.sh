cc3314f2776637f8e68b131777696c7f02c2acce
Onyinye <onychukka@gmail.com>
2023-09-12 19:00:47 -0400

    Performs a protein-nucleotide blast search for Pseudomonas_aeruginosa_UCBPP-PA14

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-tblastn -query "HK_domain.faa" -subject "Escherichia_coli_K12.fna" -task tblastn -outfmt '6 std qlen' | awk '$3 > 30  && $4 > 0.9*$13' >> hits_Escherichia_coli_K12.fna
-wc -l hits_Escherichia_coli_K12.fna
-#head hits_Escherichia_coli_K12.fna
+tblastn -query "HK_domain.faa" -subject "Pseudomonas_aeruginosa_UCBPP-PA14.fna" -task tblastn -outfmt '6 std qlen' | awk '$3 > 30  && $4 > 0.9*$13' >> hits_Pseudomonas_aeruginosa_UCBPP-PA14.fna
+wc -l hits_Pseudomonas_aeruginosa_UCBPP-PA14.fna
+#head hits_Pseudomonas_aeruginosa_UCBPP-PA14.fna
 
